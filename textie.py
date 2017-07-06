395c506c5a587ca40601c8df2f9031e39e00241e
kaumilshah <13bit049@nirmauni.ac.in>
2017-07-06 13:09:19 +0530

    defirhf

--- textie.py
+++ textie.py
@@ -44,7 +44,7 @@ def messaging_events(payload):
 def send_message(token, recipient, text):
   """Send the message text to recipient with id recipient.
   """
-  print ("helloooo :" ,(text))
+  print ("helloooo :" ,text.decode('unicode_escape'))
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
     data=json.dumps({
