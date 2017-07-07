1a1b792928acc1ba4aa308671d79f96e6c3238ba
kaumilshah <13bit049@nirmauni.ac.in>
2017-07-07 11:03:52 +0530

    hey

--- textie.py
+++ textie.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import json
 import requests
-
+import random
 
 #nltk lib
 import nltk
@@ -11,65 +11,8 @@ import json
 import datetime
 
 
-app = Flask(__name__)
 
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
-PAT = 'EAADZA70JUHVUBAMQRPGBgc5PJGX22keHuGv4CGQr8NU1Dr94Xv7E9vCKNL24KGch91U2XzHaZAj8SR2jgj2CaePmyaBsEdg6TE2XDiIZBqkJ5qVESplnGlowYNMdi4c82zwGvhTTN57eLIElI0THg9F5Cv5b3mB0w03rF8fglTR3RstpecG'
-
-@app.route('/', methods=['GET'])
-def handle_verification():
-  print ("Handling Verification.")
-  if request.args.get('hub.verify_token', '') == '260117':
-    print ("Verification successful!")
-    return request.args.get('hub.challenge', '')
-  else:
-    print ("Verification failed!")
-    return 'Error, wrong validation token'
-
-@app.route('/', methods=['POST'])
-def handle_messages():
-  print ("Handling Messages")
-  payload = request.get_data()
-  print (payload)
-  for sender, message in messaging_events(payload):
-    print ("Incoming from %s: %s" % (sender, message))
-    send_message(PAT, sender, message)
-  return "ok"
-
-def messaging_events(payload):
-  """Generate tuples of (sender_id, message_text) from the
-  provided payload.
-  """
-  data = json.loads(payload)
-  messaging_events = data["entry"][0]["messaging"]
-  for event in messaging_events:
-    if "message" in event and "text" in event["message"]:
-      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-    else:
-      yield event["sender"]["id"], "I can't echo this"
-
-
-def send_message(token, recipient, text):
-  """Send the message text to recipient with id recipient.
-  """
-  print ("helloooo :" ,text.decode('unicode_escape'))
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
-    }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print (r.text)
-
-if __name__ == '__main__':
-  app.run()
-  
-
-
-
+#neural network________________________________________________________________________________-
 stemmer = LancasterStemmer()
 training_data = []
 training_data.append({"class":"greeting", "sentence":"how are you?"})
@@ -82,10 +25,9 @@ training_data.append({"class":"goodbye", "sentence":"see you later"})
 training_data.append({"class":"goodbye", "sentence":"have a nice day"})
 training_data.append({"class":"goodbye", "sentence":"talk to you soon"})
 
-training_data.append({"class":"sandwich", "sentence":"make me a sandwich"})
-training_data.append({"class":"sandwich", "sentence":"can you make a sandwich?"})
-training_data.append({"class":"sandwich", "sentence":"having a sandwich today?"})
-training_data.append({"class":"sandwich", "sentence":"what's for lunch?"})
+training_data.append({"class":"mood", "sentence":"I am feeling sad"})
+training_data.append({"class":"mood", "sentence":"Its been a nice day"})
+training_data.append({"class":"mood", "sentence":"I am on cloud 9"})
 print ("%s sentences in training data" % len(training_data))
 
 words = []
@@ -300,14 +242,86 @@ def classify(sentence, show_details=False):
     print ("%s \n classification: %s" % (sentence, return_results))
     return return_results
 
-classify("sudo make me a sandwich")
-classify("how are you today?")
-classify("talk to you tomorrow")
-classify("who are you?")
-classify("make me some lunch")
-classify("how was your lunch today?")
+
 print()
-classify("good day", show_details=True)
+
+def outputfun(msg):
+    res = classify(msg)
+    if res.equals("greetings"):
+       return random.choice(["Hey , nice to meet you!","Hello, how you doing ?","Its been a great day ,how was yours?","Tell me your story.","How can I help you? "])
+    elif res.equals("mood"):
+        return random.choice(["More you laugh , more you live !!","If you dont know where to go then it doesn't matter which path you choose.","Live your life like when you die...make sure that you may die with memories...not with dreams...!","In every story there is a possibility of happy ending.. but it depends where we want the story to end!"])
+    elif res.equals("goodbye"):
+        return random.choice(["it was nice talking to you ","signing off now!","see yaa,talk to you later "])
+    else:
+        return random.choice(["sorry,I didn't get it! ","Can you be more specific?"])
+
+
+#________________________________________________________________________________
+
+
+app = Flask(__name__)
+
+# This needs to be filled with the Page Access Token that will be provided
+# by the Facebook App that will be created.
+PAT = 'EAADZA70JUHVUBAMQRPGBgc5PJGX22keHuGv4CGQr8NU1Dr94Xv7E9vCKNL24KGch91U2XzHaZAj8SR2jgj2CaePmyaBsEdg6TE2XDiIZBqkJ5qVESplnGlowYNMdi4c82zwGvhTTN57eLIElI0THg9F5Cv5b3mB0w03rF8fglTR3RstpecG'
+
+@app.route('/', methods=['GET'])
+def handle_verification():
+  print ("Handling Verification.")
+  if request.args.get('hub.verify_token', '') == '260117':
+    print ("Verification successful!")
+    return request.args.get('hub.challenge', '')
+  else:
+    print ("Verification failed!")
+    return 'Error, wrong validation token'
+
+@app.route('/', methods=['POST'])
+def handle_messages():
+  print ("Handling Messages")
+  payload = request.get_data()
+  print (payload)
+  for sender, message in messaging_events(payload):
+    print ("Incoming from %s: %s" % (sender, message))
+    send_message(PAT, sender, message)
+  return "ok"
+
+def messaging_events(payload):
+  """Generate tuples of (sender_id, message_text) from the
+  provided payload.
+  """
+  data = json.loads(payload)
+  messaging_events = data["entry"][0]["messaging"]
+  for event in messaging_events:
+    if "message" in event and "text" in event["message"]:
+      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+    else:
+      yield event["sender"]["id"], "I can't echo this"
+
+
+def send_message(token, recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+  print ("helloooo :" ,text.decode('unicode_escape'))
+
+  ot=outputfun(text.decode('unicode_escape'))  
+  
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {"text": ot}
+    }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print (r.text)
+
+if __name__ == '__main__':
+  app.run()
+  
+  
+
+
 
 
 
